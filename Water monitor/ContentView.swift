eabe1cc9bd0dc45790fdb35023cb364fc14c6b18
Tanatswa <mabungutanatswa2018@gmail.com>
2025-10-16 14:18:23 +0200

    changed code from the default code to the welcome page code

--- Water monitor/ContentView.swift
+++ Water monitor/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                Spacer()
+                Image("logo")
+                    .resizable()
+                    .frame(width: 400, height: 400)
+                Text("AquaTrack")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("Monitor  usage, reduce waste and enjoy a more sustainable lifestyle!")
+                Spacer()
+                NavigationLink("Get started"){ HomePage()
+                    
+                }
+                .padding()
+                .foregroundStyle(.black)
+                .fontWeight(.bold)
+                .background(Capsule().fill(Color.blue))
+            }
         }
-        .padding()
     }
 }
 
---
+++ Water monitor/HomePage.swift
@@ -0,0 +1,18 @@
+//
+//  HomePage.swift
+//  Water monitor
+//
+//  Created by Tana on 16/10/2025.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePage()
+}
