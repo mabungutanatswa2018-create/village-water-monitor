2fe6951391760156a700186b3e5e68b5f3444378
Tanatswa <mabungutanatswa2018@gmail.com>
2025-10-16 12:44:34 +0200

    Initial Commit

---
+++ Water monitor/Water_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_monitorApp.swift
+//  Water monitor
+//
+//  Created by Tana on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Water_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Water monitorTests/Water_monitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Water_monitorTests.swift
+//  Water monitorTests
+//
+//  Created by Tana on 16/10/2025.
+//
+
+import Testing
+@testable import Water_monitor
+
+struct Water_monitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
