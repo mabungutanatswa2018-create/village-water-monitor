79616b44927b5d9e6e5e9e1a06c7158e66f9dcd9
Tanatswa <mabungutanatswa2018@gmail.com>
2025-10-20 14:09:31 +0200

    changed the default code and wrote the code for the home page. There is a list of names. There is also a navigation link that takes the user to the details page and a code that is used to slide a name inorder to delete.

--- Water monitor/HomePage.swift
+++ Water monitor/HomePage.swift
@@ -8,11 +8,81 @@
 import SwiftUI
 
 struct HomePage: View {
+    @State private var names = ["Rhodes", "Miller", "Willing", "Harrison","Johnson", "Smith", "Moyo", "Ncube", "Hill", "Wilson", "Shumba", "Ndlovu", "Warner", "Ewart", "Brown", "White", "Jones", "Washington", "Phiri", "Patil"]
+    @State var newName: String = ""
+    @State private var showTextField = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack {
+                
+                HStack {
+                    Text("Family names")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding()
+                    Spacer()
+                    
+                }
+                .padding(.trailing, 20)
+                
+                List{ ForEach(names, id: \.self){name in
+                   
+                   NavigationLink(destination: Details()) {
+                    Text(name)
+                    }
+                        
+                }
+                
+                .onDelete(perform: deleteName)
+                
+                }
+                if showTextField {
+                    HStack {
+                        TextField("Enter family name", text: $newName)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.leading)
+                        
+                        Button(action:{
+                            if !newName.isEmpty {
+                                names.append(newName)
+                                newName = ""
+                                showTextField.toggle()
+                            }
+                            
+                        }){
+                            Image(systemName: "checkmark.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+                
+                //animation of a button and plus button
+                Button(action:{
+                    withAnimation{
+                        showTextField.toggle()
+                    }
+                    
+                }){
+                    Image(systemName: showTextField ? "xmark.circle.fill" : "plus.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.blue)
+                        .padding()
+                    
+                }
+                
+                
+            }
+        }
     }
+    //deleting a name
+        func deleteName(at offsets: IndexSet){
+            names.remove(atOffsets: offsets)
+        }
+    
 }
-
 #Preview {
     HomePage()
 }
