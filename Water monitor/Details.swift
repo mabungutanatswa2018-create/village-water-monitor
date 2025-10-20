db9c5a6ee89ee2ada4840658b70ad497e99d83cd
Tanatswa <mabungutanatswa2018@gmail.com>
2025-10-20 14:01:17 +0200

    added a water logo

---
+++ Water monitor/Details.swift
@@ -0,0 +1,18 @@
+//
+//  Details.swift
+//  Water monitor
+//
+//  Created by Tana on 20/10/2025.
+//
+
+import SwiftUI
+
+struct Details: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Details()
+}
